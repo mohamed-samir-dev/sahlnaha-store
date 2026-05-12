1585c73a881bf698a75ae4872fa11aa488b577f3
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-05-13 01:15:28 +0300

    First

--- app/components/navbar/data/navData.ts
+++ app/components/navbar/data/navData.ts
@@ -34,8 +34,8 @@ export const navItems: NavItem[] = [
           { label: "آيفون 15 برو ماكس", href: "/smartphones/iphone-15-pro-max" },
           { label: "آيفون 15 بلس", href: "/smartphones/iphone-15-plus" },
           { label: "آيفون 14 برو ماكس", href: "/smartphones/iphone-14-pro-max" },
-          { label: "آيفون 14 برو", href: "/smartphones/iphone-14-pro" },
-          { label: "فقط آبل", href: "/smartphones/apple-only" },
+          // { label: "آيفون 14 برو", href: "/smartphones/iphone-14-pro" },
+          // { label: "فقط آبل", href: "/smartphones/apple-only" },
         ],
       },
       {
