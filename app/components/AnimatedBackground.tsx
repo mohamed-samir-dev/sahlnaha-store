e95fdb8b8cc092abe8990561deeafc02e8e55c7b
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-05-20 15:24:04 +0300

    Add

--- app/components/AnimatedBackground.tsx
+++ app/components/AnimatedBackground.tsx
@@ -3,10 +3,28 @@
 export default function AnimatedBackground() {
   return (
     <div className="animated-bg fixed inset-0 -z-10 overflow-hidden">
+      {/* Blobs */}
       <div className="blob blob-1" />
       <div className="blob blob-2" />
       <div className="blob blob-3" />
       <div className="blob blob-4" />
+
+      {/* Aurora rays */}
+      <div className="aurora aurora-1" />
+      <div className="aurora aurora-2" />
+      <div className="aurora aurora-3" />
+
+      {/* Floating particles */}
+      <div className="particle p1" /><div className="particle p2" />
+      <div className="particle p3" /><div className="particle p4" />
+      <div className="particle p5" /><div className="particle p6" />
+      <div className="particle p7" /><div className="particle p8" />
+      <div className="particle p9" /><div className="particle p10" />
+      <div className="particle p11" /><div className="particle p12" />
+
+      {/* Scan line */}
+      <div className="scan-line" />
+
       <div className="grid-overlay" />
       <div className="top-shine" />
     </div>
